284230d84c17a9fe39ac38cbb9e981c1a2c5b282
suleim5 <139492602+suleim5@users.noreply.github.com>
2024-12-13 14:31:20 +0300

    Update bruteforce.cmd

--- Source Code/bruteforce.cmd
+++ Source Code/bruteforce.cmd
@@ -395,10 +395,11 @@
 			if %%a==SSID (
 				set /a keynumber=!keynumber! + 1
 				set current_ssid=%%d
+                    if "!current_ssid!"=="" (
+    set "current_ssid=Hidden_Network"
+)
 
-				if "!current_ssid!=="" (
-					set "current_ssid=Hidden_Network"
-				)
+				
 
 				call :character_finder_2 "!current_ssid!"
 
